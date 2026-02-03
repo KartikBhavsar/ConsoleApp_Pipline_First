e355faba3578d16723e8e61f2d6d1135b51e7db0
Kartik <kartikbhavsar333@gmail.com>
2026-02-03 11:58:42 +0530

    Add project files.

---
+++ ConsoleApp_Pipline_First/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Pipline_First
+{
+    internal class Program
+    {
+        public void First()
+        {
+            Console.WriteLine("Hello Pipeline First Project K");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.First();
+        }
+    }
+}
